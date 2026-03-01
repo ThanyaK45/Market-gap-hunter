de11ed54a43f1c3245679fbf625f7f0dc93bcde2
Thanyanon <thanyatle24@hotmail.com>
2026-03-01 16:01:35 +0700

    Fix pic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react'
 import axios from 'axios'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import toast, { Toaster } from 'react-hot-toast'
+import L from 'leaflet'
 import SearchBox from './components/SearchBox'
 import MapView from './components/MapView'
 import AnalysisControls from './components/AnalysisControls'
 import ResultCard from './components/ResultCard'
 import 'leaflet/dist/leaflet.css'
 import './App.css'
+
+// Fix Leaflet marker icons for production
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
